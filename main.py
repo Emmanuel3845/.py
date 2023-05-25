5da4c97715ac3640dadc16590f3d86c16b901052
Emmanuel3845 <134169772+Emmanuel3845@users.noreply.github.com>
2023-05-25 13:22:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-username = input("Username:")
-password = input("Enter Password:")
-confirm_password = input("Confirm Password:")
+username = input("What is your name: ")
+password = input("Insert Password: ")
+confirm_password = input("Confirm Password: ")
 
 if password == confirm_password:
-    print('password confirmed')
+    print("correct password")
 else:
     while password != confirm_password:
-        print('password incorrect')
-        confirm_password = input("Retype Password")
+        print("retry password")
+        password = input("Insert Password: ")
+        confirm_password = input("Confirm Password: ")
+
+    if password == confirm_password:
+        print("correct password")
+
+# login
+
+login_username = input("What is your login name: ")
+login_password = input("Insert Login Password: ")
+confirm_loginpassword = input("Confirm Login Password: ")
+
+if password == confirm_loginpassword:
+    print("correct password")
+else:
+    while password != confirm_loginpassword:
+        print("retry password")
+        password = input("Insert login Password: ")
+        confirm_loginpassword = input("Confirm Password: ")
+
+    if password == confirm_loginpassword:
+        print("login password correct ")
